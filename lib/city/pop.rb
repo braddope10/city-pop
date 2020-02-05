80012a27f00162375f8886d2290d7e40e0676f5f
github username <github email address>
2020-02-05 14:56:06 -0800

    modified version

---
+++ lib/city/pop.rb
@@ -0,0 +1,8 @@
+require "city/pop/version"
+
+module City
+  module Pop
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/city/pop/version.rb
@@ -0,0 +1,7 @@
+module City
+  module Pop
+    VERSION = "0.1.0"
+  end
+end
+
+#what the fuck
